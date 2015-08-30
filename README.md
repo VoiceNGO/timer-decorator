073d632d26bb821f5098b0f32595c9a5914607ed
Mark Kahn <m@vouchr.com>
2015-08-30 15:09:04 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Timer Decorator
+
+A simple Promise-aware ES7 timer decorator.  Just logs the processing time for
+  the given function, or the time until the promise resolves (ignores rejected
+  promises).  Note that the promise itself adds ~35-40ms in my basic testing.
+
+## Usage
+
+```js
+import timer from 'timer-decorator';
+
+class Foo {
+  @timer;
+  myMethod() {
+    // ...
+  }
+}
+```
+
+will log something like:
+
+```
+Foo[0].myMethod: 123.456ms
+```
+
+The `[0]` is the instance #.
+
+### Enable and Disable
+
+To enable or disable the timer, call:
+
+```js
+timer.enable(this);
+timer.disable(this);
+```
+
+where `this` is the class instance.
+
+### Release
+
+```js
+timer.release(this);
+```
+
+to release the object and allow for gc to clean it.  Note that you probably
+  shouldn't be using this in production anyway, so it's somewhat irrelevant if
+  you don't do this (I'm just writing it for you to be aware that you will
+  keep around _every_ object that has a timer in memory, forever, if `release`
+  isn't called).
---
+++ index.js
@@ -0,0 +1,105 @@
+const WEAKMAP = typeof WeakMap !== 'undefined';
+
+let instances = WEAKMAP ? new WeakMap() : [];
+let counter   = 0;
+
+function addToMap(instance) {
+  let inst = {enabled: true, index: counter++};
+  instances.set(instance, inst);
+
+  return inst;
+}
+
+function getIndex(instance) {
+  if (WEAKMAP) {
+    return (instances.get(instance) || addToMap(instance)).index;
+  }
+
+  for (let i=0, l=instances.length; i<l; i++) {
+    if (instances[i].obj === instance) {
+      return i;
+    }
+  }
+
+  return instances.push({obj: instance, enabled: true}) - 1;
+}
+
+function toggle(instance, enabled) {
+  if (WEAKMAP) {
+    (instances.get(instance) || addToMap(instance)).enabled = enabled;
+  } else {
+    instances[getIndex(instance)].enabled = enabled;
+  }
+}
+
+function isEnabled(instance) {
+  if (WEAKMAP) {
+    return instances.has(instance) ? instances.get(instance).enabled : addToMap(instance).enabled;
+
+  } else {
+    return !!(instances[getIndex(instance)] || {}).enabled;
+  }
+}
+
+function isPromise(obj) {
+  return !!obj && (typeof obj === 'object' || typeof obj === 'function') && typeof obj.then === 'function';
+}
+
+module.exports = function(target, key, descriptor) {
+  let fn = descriptor.value;
+
+  descriptor.value = function() {
+
+    if (isEnabled(this)) {
+      let instanceNum = getIndex(this);
+      let timingName  = `${this.constructor.name}#${instanceNum}.${key}`;
+      let self        = this;
+      let args        = arguments;
+      let retVal      = null;
+
+      console.time(timingName);
+      new Promise(function(resolve, reject) {
+        retVal = fn.apply(self, args);
+
+        if (isPromise(retVal)){
+          resolve(retVal);
+        } else {
+          console.timeEnd(timingName);
+
+          reject();
+        }
+      }).then(
+        console.timeEnd.bind(console, timingName)
+      ).catch(() => {});
+
+      return retVal;
+
+    } else {
+      return fn.call(this, arguments);
+    }
+  };
+
+  return descriptor;
+};
+
+module.exports.enable = function(instance) {
+  toggle(instance, true);
+
+  return this;
+};
+
+module.exports.disable = function(instance) {
+  toggle(instance, false);
+
+  return this;
+};
+
+module.exports.release = function(instance) {
+  if (WEAKMAP) {
+    instances.delete(instance);
+  } else {
+    instances[getIndex(instance)] = null;
+  }
+
+  return this;
+};
